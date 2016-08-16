843f4152a160010f49f518db470128812c358766
be-zet <pionier666@gmail.com>
2016-08-16 19:26:37 +0200

    Up

--- createvhost.py
+++ createvhost.py
@@ -1,58 +1,182 @@
 #!/usr/bin/python
 
-import sys, getopt
+import os, sys, getopt, json
+# import subprocess, webbrowser
+
+
+def createConfigFile():
+
+    config = [
+        '{',
+        '   "hostsFile": "C:/Windows/System32/drivers/etc/hosts",',
+        '   "httpdHostsFile": "C:/wamp64/bin/apache/apache2.4.18/conf/extra/httpd-vhosts.conf"',
+        '}'
+    ]
+
+    config = '\n'.join(config)
+
+    configFile = open('config.json', 'w')
+    configFile.write(config)
+    configFile.close()
+
+    print("Provide paths to your hosts and httpd-vhosts.conf files.")
+    editFile('config.json')
+
+
+def editFile(filePath, editor="notepad.exe"):
+    
+    # webbrowser.open(filePath)
+    # subprocess.Popen([editor, filePath])
+    os.system(editor + " " + filePath)
+
+
+def createVirtualHost(hostsFile, httpdHostsFile, hostname, projectDirectory):
+    
+    hostsFile.write('\n' + '127.0.0.1       ' + hostname)
+
+    ## This is a working example of vhost, just in case  
+    # <VirtualHost *:80>
+    #   ServerName virtualhost.local
+    #   ServerAlias virtualhost.local
+    #   DocumentRoot "C:/wamp/www/virtualhostdir"
+    #   <Directory "C:/wamp/www/virtualhostdir/">
+    #       Options Indexes FollowSymLinks MultiViews
+    #       AllowOverride all
+    #       Require all granted
+    #       Require local
+    #   </Directory>
+    # </VirtualHost>
+    
+    vhostDef = [
+        '  <VirtualHost *:80>',
+        '    ServerName ' + hostname,
+        '    ServerAlias ' + hostname,
+        '    DocumentRoot "' + projectDirectory + '"',
+        '    <Directory "' + projectDirectory + '/">',
+        '      Options Indexes FollowSymLinks MultiViews',
+        '      AllowOverride all',
+        '      Require all granted',
+        '      Require local',
+        '    </Directory>',
+        '  </VirtualHost>',
+    ]
+
+    vhostDef = '\n'.join(vhostDef)
+    
+    httpdHostsFile.write('\n\n' + vhostDef)
+
+
+def usage():
+    
+    usageText = [
+        ' ',
+        'usage:',
+        '    python ' + sys.argv[0] + ' [-c | --config] [[-n | --name=<hostname>] [-d | --directory=<project-directory>]]',
+        ' ',
+        'examples:',
+        '    python ' + sys.argv[0] + ' --config',
+        '        Pops up notepad.exe with Windows hosts & HTTP server vhosts config (vhosthttpd-vhosts.conf) filepaths configuration.',
+        ' ',
+        '    python ' + sys.argv[0] + ' --config --name newhostname --directory C:/project',
+        '        Configure paths and set up a new local virtual host. If you provide a name without dot, .local is added to the final url.',
+        ' ',
+        '    python ' + sys.argv[0] + ' --name newhostname --directory C:/project',
+        '        If configuration is already done, it simply sets up a new virtual host. If config doesn\'t exist, notepad.exe pops up anyway.'
+    ]
+
+    usageText = '\n'.join(usageText)
+
+    print(usageText)
 
 
 def main(argv):
-	hostsFile = open('C:/Windows/System32/drivers/etc/hosts', 'a')
-	httpdHostsFile = open('C:/wamp/bin/apache/apache2.4.9/conf/extra/httpd-vhosts.conf', 'a')
-	newVHost = argv[0]
-	projectDir = argv[1]
 
-	if newVHost.rfind('.') == -1:
-		newVHost = newVHost + '.local'
-		
-	print('Creating new virtual host:', newVHost)
-	
-	hostsFile.write('\n' + '127.0.0.1       ' + newVHost)
+    if len(argv) == 0:
+        print("Rerun script with arguments and/or options.")
+        usage()
+        sys.exit()
 
-	# <VirtualHost *:80>
-	# 	ServerName baza.local
-	# 	ServerAlias baza.local
-	# 	DocumentRoot "C:/wamp/www/git/portfolio"
-	# 	<Directory "C:/wamp/www/git/portfolio/">
-	# 		Options Indexes FollowSymLinks MultiViews
-	# 		AllowOverride all
-	# 		Require all granted
-	# 		Require local
-	# 	</Directory>
-	# </VirtualHost>
-	
-	vhostDef = [
-		'  <VirtualHost *:80>',
-		'    ServerName ' + newVHost,
-		'    ServerAlias ' + newVHost,
-		'    DocumentRoot "'+ projectDir +'"',
-		'    <Directory "'+ projectDir +'/">',
-		'      Options Indexes FollowSymLinks MultiViews',
-		'      AllowOverride all',
-		'      Require all granted',
-		'      Require local',
-		'    </Directory>',
-		'  </VirtualHost>',
-	]
+    try:
+        opts, args = getopt.getopt(argv, 'hcn:d:', ['help', 'config', 'name=', 'directory='])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
 
-	vhostDef = '\n'.join(vhostDef)
-	
-	httpdHostsFile.write('\n\n' + vhostDef)
-	
-	hostsFile.close()
-	httpdHostsFile.close()
+    hostname = None
+    projectDirectory = None
 
-	print('Done!')
-	print('Project\'s directory:', projectDir)
-	print('Your new url:', 'http://' + newVHost)
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            usage()
+            sys.exit()
+
+        if opt in ('-c', '--config'):
+            if not os.path.isfile('./config.json'):
+                createConfigFile()
+            else:
+                editFile('config.json')
+
+            print("Config is set.")
+
+        if opt in ('-n', '--name'):
+            if len(arg) > 0:
+                hostname = arg
+
+                if hostname.rfind('.') == -1:
+                    hostname = hostname + '.local'
+            else:
+                print("Name option value is empty.")
+                sys.exit()
+
+        if opt in ('-d', '--directory'):
+            if os.path.isdir(arg):
+                projectDirectory = arg
+            else:
+                print("Provided directory path doesn't exist. Make sure it's valid or create directory first and rerun the script.")
+                sys.exit()
+
+    if hostname is None or projectDirectory is None:
+        print("You're doing it wrong.")
+        usage()
+        sys.exit()
+
+    if not os.path.isfile('./config.json'):
+        createConfigFile()
+
+    while 1:
+        try:
+            with open('config.json') as configFile:
+                config = json.load(configFile)
+
+            hostsFile = open(config['hostsFile'], 'a')
+            httpdHostsFile = open(config['httpdHostsFile'], 'a')
+
+            print("Config is set.")
+            break
+
+        except FileNotFoundError:
+            keyInput = input("File paths provided in your config are invalid - do you want to edit config file? y/n \n")
+
+            if keyInput == 'y' or keyInput == 'yes':
+                editFile('config.json')
+            elif keyInput == 'n' or keyInput == 'no':
+                sys.exit()
+        
+    print('Creating a new virtual host:', hostname)
+    createVirtualHost(hostsFile, httpdHostsFile, hostname, projectDirectory)
+    
+    hostsFile.close()
+    httpdHostsFile.close()
+
+    url = 'http://' + hostname
+
+    print("Done!")
+    print("Project's directory:", projectDirectory)
+    print("Your new url:", url)
+    print("You might need to restart your HTTP server.")
+
+    # webbrowser.open(url)
 
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
--- readme.md
+++ readme.md
@@ -1,1 +1,14 @@
-Script creating virtual hosts (wamp, windows 7)  
+Script creating local virtual hosts (Windows 7, Apache)
+
+usage:
+	python createvhost.py [-c | --config] [[-n | --name=<hostname>] [-d | --directory=<project-directory>]]
+
+examples:
+	python createvhost.py --config
+	    Pops up notepad.exe with Windows hosts & web server vhosts config (vhosthttpd-vhosts.conf) filepaths configuration.
+         
+	python createvhost.py --config --name newhostname --directory C:/project
+	    Configure paths and set up a new virtual host. If you provide name without dot, .local is added to the final url.
+         
+	python createvhost.py --name newhostname --directory C:/project
+        If configuration is already done, it simply sets up a new virtual host. If config doesn\t exist, notepad.exe pops up anyway.
